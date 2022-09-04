3ce341a84a098634ce442e5030fcbf1cd80bd28b
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-04 16:21:57 +0900

    mvc구조로 변경

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path')
-const adminData = require('./admin');
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const produncts = adminData.products;
-    res.render('shop', { prods: produncts, pageTitle: 'Shop', path: '/', hasProducts: produncts.length > 0, activeShop: true, productCSS: true });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
