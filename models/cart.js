d387005cb09ad29b097b5e04a1d5f910f11ed568
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-05 19:48:09 +0900

    장바구니 추가 기능

---
+++ models/cart.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, filecontent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(filecontent);
+            }
+            const existingProductIndex = cart.products.findIndex(product => product.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updateProduct;
+            if (existingProduct) {
+                updateProduct = [...existingProduct];
+                updateProduct.qty = updateProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updateProduct;
+            } else {
+                updateProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updateProduct];
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            });
+        })
+    }
+}
