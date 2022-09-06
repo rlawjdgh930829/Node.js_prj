6017eb74180da656b94aac851fca98b8dd849a74
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-06 14:42:35 +0900

    상품수정 기능 추가

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,41 @@
 const Product = require('../models/product');
 
 exports.getAddproduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        editing: false
     });
+}
+
+exports.getEditAddproduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode) {
+        return res.redirect('/');
+    }
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    }); 
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const productId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedProduct = new Product(productId, updatedTitle, updatedImageUrl, updatedPrice, updatedDescription);
+    updatedProduct.save();
+    res.redirect('/admin/products');
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -12,8 +43,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null ,title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 }
