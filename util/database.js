bb88eb0d83fa53d3c2bd19064cf27012b4b4d7a0
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-07 15:58:10 +0900

    sequelize 연동

--- util/database.js
+++ util/database.js
@@ -1,11 +1,6 @@
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', host: process.env.DB_HOST});
 
-module.exports = pool.promise();
+module.exports = sequelize;
