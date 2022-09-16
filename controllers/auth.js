74a17dda0f4bbb7966e6d3970b112eeac93fdc5b
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-16 16:36:24 +0900

    이메일을 통해 비밀번호 변경

--- controllers/auth.js
+++ controllers/auth.js
@@ -127,14 +127,14 @@ exports.getReset = (req, res, next) => {
 
 exports.postReset = (req, res, next) => {
     crypto.randomBytes(32, (err, buffer) => {
-        if(err) {
+        if (err) {
             console.log(err);
             return res.redirect('/reset');
         }
         const token = buffer.toString('hex');
-        User.findOne({email: req.body.email})
+        User.findOne({ email: req.body.email })
             .then(user => {
-                if(!user) {
+                if (!user) {
                     req.flash('error', 'No account with that email found');
                     return res.redirect('/reset');
                 }
@@ -156,4 +156,47 @@ exports.postReset = (req, res, next) => {
             })
             .catch(err => console.log(err));
     });
+};
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+        .then(user => {
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+    User.findOne({ resetToken: passwordToken, resetTokenExpiration: { $gt: Date.now() }, _id: userId })
+        .then(user => {
+            resetUser = user;
+            return bcrypt.hash(newPassword, 12);
+        })
+        .then(hashedPassword => {
+            resetUser.password = hashedPassword;
+            resetUser.resetToken = undefined;
+            resetUser.resetTokenExpiration = undefined;
+            return resetUser.save();
+        })
+        .then(result => {
+            res.redirect('login');
+        })
+        .catch(err => console.log(err));
 }
