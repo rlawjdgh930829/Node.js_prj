433eba007cc060c0bbfdb08efeb194f41465586b
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-17 18:10:54 +0900

    제품 추가, 삭제 유효성 검사

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,14 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator');
 
 exports.getAddproduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false
+        editing: false,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
     });
 }
 
@@ -23,7 +27,10 @@ exports.getEditAddProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product
+                product: product,
+                hasError: false,
+                errorMessage: null,
+                validationErrors: []
             });
         })
         .catch(err => console.log(err));
@@ -35,6 +42,25 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
     const updatedImageUrl = req.body.imageUrl;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: true,
+            hasError: true,
+            product: {
+                title: updatedTitle,
+                imageUrl: updatedImageUrl,
+                price: updatedPrice,
+                description: updatedDescription,
+                _id: productId
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
     Product.findById(productId)
         .then(product => {
             if (product.userId.toString() !== req.user._id.toString()) {
@@ -58,6 +84,24 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
     const imageUrl = req.body.imageUrl;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: []
+        });
+    }
+
     const product = new Product({
         title: title,
         price: price,
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
@@ -9,11 +10,25 @@ router.get('/add-product', isAuth, adminController.getAddproduct);
 
 router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product',
+    [
+        body('title').isString().isLength({ min: 3 }).trim(),
+        body('imageUrl').isURL(),
+        body('price').isFloat(),
+        body('description').isLength({ min: 5, max: 400 }).trim()
+    ],
+    isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditAddProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product',
+    [
+        body('title').isString().isLength({ min: 3 }).trim(),
+        body('imageUrl').isURL(),
+        body('price').isFloat(),
+        body('description').isLength({ min: 5, max: 400 }).trim()
+    ],
+    isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
