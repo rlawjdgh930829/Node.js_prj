dc0f21f7cbc89f8ffb69bf3792b37ceb6af8e097
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-20 22:52:57 +0900

    stripe를 이용한 결제 테스트

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.SECRET_KEY);
+
 
 const PDFDocument = require('pdfkit');
 
@@ -132,7 +135,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
         });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckoutSuccess = (req, res, next) => {
     req.user.populate('cart.items.productId')
         .then(user => {
             const products = user.cart.items.map(i => {
@@ -152,6 +155,52 @@ exports.postOrder = (req, res, next) => {
         })
         .then(() => {
             res.redirect('/orders');
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+}
+
+exports.getCheckout = (req, res, next) => {
+    let products;
+    let total;
+    req.user.populate('cart.items.productId')
+        .then(user => {
+            products = user.cart.items;
+            total = 0;
+            products.forEach(p => {
+                total += p.productId.price * p.quantity;
+            });
+            return stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            unit_amount: p.productId.price * 100,
+                            product_data: {
+                                name: p.productId.title,
+                                description: p.productId.description
+                            }
+                        },
+                        quantity: p.quantity
+                    };
+                }),
+                mode: 'payment',
+                success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+            });
+        })
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id
+            });
         })
         .catch(err => {
             const error = new Error(err);
